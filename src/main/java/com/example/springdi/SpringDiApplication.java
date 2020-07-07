2a9191d8fab55689c65170aa1dd9b9424c21d569
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-07 22:35:27 +0530

    done

--- src/main/java/com/example/springdi/SpringDiApplication.java
+++ src/main/java/com/example/springdi/SpringDiApplication.java
@@ -2,6 +2,8 @@ package com.example.springdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringDiApplication {
@@ -9,11 +11,18 @@ public class SpringDiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDiApplication.class, args);
 
-        //  Using Book
+        //  Get the Spring context created by XML file
+        ApplicationContext context = new
+                ClassPathXmlApplicationContext( "beans.xml" );
 
-        //  This is more dynamic and customizable
-        Book book1 = new Book(new FictionAuthor());
-        Book book2 = new Book(new TechAuthor());
+
+        //  Get the dependencies by bean id
+        Author fictionAuthor = context.getBean("fiction", Author.class);
+        Author techAuthor = context.getBean("tech", Author.class);
+
+        //  Using the dependencies
+        Book book1 = new Book(fictionAuthor);
+        Book book2 = new Book(techAuthor);
 
         //  What about authors?
         book1.getAuthor().getDetails();
