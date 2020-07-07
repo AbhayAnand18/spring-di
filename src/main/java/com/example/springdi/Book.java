d1b3d2ba4a068cb9fecef0674b8baa1ae608776b
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-07 21:17:15 +0530

    done

--- src/main/java/com/example/springdi/Book.java
+++ src/main/java/com/example/springdi/Book.java
@@ -5,7 +5,7 @@ public class Book {
 
     //  Rather than this class creates its dependencies
     //  it get the dependencies "provided"
-    //  This is called as Dependency Inversion or
+    //  This is called as Dependency Injection or
     //  Inversion of Control (control stands for ability to create)
     //  This way, Book can be provided with different Author objects
     public Book(Author a) {
