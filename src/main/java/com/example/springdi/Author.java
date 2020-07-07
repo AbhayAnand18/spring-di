3cb4b76e06f1aa977675206080a851456602a1cf
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-07 21:12:46 +0530

    done

--- src/main/java/com/example/springdi/Author.java
+++ src/main/java/com/example/springdi/Author.java
@@ -1,17 +1,4 @@
 package com.example.springdi;
 
 public class Author {
-    private String name;
-
-    public Author(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
